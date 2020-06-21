29600dd915d496551e98b5df18a82e1b2ca9a3ca
niru_ <niranjan.reddy.wo@gmail.com>
2020-06-21 16:05:30 +0530

    adding docs and setup file

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. imusensor documentation master file, created by
+   sphinx-quickstart on Sun Jun 21 16:03:07 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to imusensor's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imusensor",
+    version="1.0.0",
+    author="Niranjan Kukkala",
+    author_email="niranjan.reddy.wo@gmail.com",
+    description="Linking Raspberry pi with MPU9250",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/niru-5/imu-mpu9250/tree/master",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
